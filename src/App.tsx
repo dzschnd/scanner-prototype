404d00e1078f34dc0fa7599a97a1adcde01f9b98
Karabas890 <ivan.mitichev@mail.ru>
2026-03-22 05:23:34 +0300

    add mock pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import { useState } from 'react'
 import ScanResultCard from './components/ScanResultCard'
 import ScannerView from './components/ScannerView'
+import AnkfCard from './components/AnkfCard';
+import LahtaCard from './components/LahtaCard';
+import { mockAnkf, mockLahta } from './mocks';
 
 function App() {
   const [scannedValue, setScannedValue] = useState<string | null>(null)
 
   if (scannedValue) {
+    if (scannedValue.startsWith('ankf:v1:')) {
+      return (
+        <AnkfCard
+          data={mockAnkf}
+          onScanAgain={() => setScannedValue(null)}
+        />
+      );
+    }
+
+    if (scannedValue.startsWith('LHT-')) {
+      return (
+        <LahtaCard
+          data={mockLahta}
+          onScanAgain={() => setScannedValue(null)}
+        />
+      );
+    }
     return (
       <ScanResultCard
-        value={scannedValue}
+        value={`Неверный формат QR-кода:\n\n${scannedValue}`}
         onScanAgain={() => setScannedValue(null)}
       />
-    )
+    );
   }
 
   return <ScannerView onScan={setScannedValue} />
